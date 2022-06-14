de84c98459bb578f068a3013d3f1e594c2418db8
blakeahalt <blakeahalt@gmail.com>
2022-06-13 23:21:05 -0700

    trying to update correct repo

--- Full Stack Open 2022/part 1/src/App.js
+++ Full Stack Open 2022/part 1/src/App.js
@@ -1,37 +1,119 @@
-const Hello = (props) => {
-       return (
-         <div>
-           <p>
-             Hello {props.name}, you are {props.age} years old
-           </p>
-         </div>
-       )
-     }
+// const Header = (props) => {
+//   return(
+//       <h1>
+//         {props.course}
+//       </h1>
+//   )
+// }
+
+// const Part = (props) => {
+//   return(
+//       <p>
+//         {props.part} {props.exercises}
+//       </p>
+//   )
+// }
+
+// const Content = (props) => {
+//   return(
+//     <div>
+//       <Part part={props.part[0].name} exercises={props.parts[0].exercises} />
+//     </div>
+//   )
+// }
+
+// const Total = (props) => {
+//   return(
+//       <p>
+//         Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} 
+//       </p>
+//     )
+//   }
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {name:'Fundamentals of React', exercises: 10},
+//       {name:'Using props to pass data', exercises: 7},
+//       {name:'State of a component', exercises: 14}
+//     ]
+//   }
+
+//   return (
+//     <div>
+//       <Header course={course.name} />
+//       <Content parts={course.parts} />
+//       <Total parts={course.parts} />
+//     </div>
+//   )
+// }
+
+// export default App
+// // ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const Header = (props) => {
+  return(
+      <h1>
+        {props.course}
+      </h1>
+  )
+}
+
+const Part = (props) => {
+  return(
+      <p>
+        {props.content.name} {props.content.exercises}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  return(
+        <p> 
+          <Part content={props.content[0]} />
+          <Part content={props.content[1]} />
+          <Part content={props.content[2]} />
+        </p> 
+  )
+}
+
+
+const Total = (props) => {
+  return(
+      <p>
+        Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises} 
+      </p>
+    )
+  }
 
 const App = () => {
-       const course = 'Half Stack application development'
-       const part1 = 'Fundamentals of React'
-       const exercises1 = 10
-       const part2 = 'Using props to pass data'
-       const exercises2 = 7
-       const part3 = 'State of a component'
-       const exercises3 = 14
-     
-       return (
-         <div>
-           <h1>{course}</h1>
-           <p>
-             {part1} {exercises1}
-           </p>
-           <p>
-             {part2} {exercises2}
-           </p>
-           <p>
-             {part3} {exercises3}
-           </p>
-           <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-         </div>
-       )
-     }
-     
-     export default App
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name:'Fundamentals of React', exercises: 10},
+      {name:'Using props to pass data', exercises: 7},
+      {name:'State of a component', exercises: 14}
+    ]
+  }
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name:'Fundamentals of React', exercises: 10},
+      {name:'Using props to pass data', exercises: 7},
+      {name:'State of a component', exercises: 14}
+    ]
+  }
+
+  return (
+    <div>
+        <Header course={course.name} />
+        <Content content={course.parts}/>
+        <Total total={course.parts} />
+    </div>
+  )
+}
+
+export default App
