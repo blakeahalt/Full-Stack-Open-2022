a9ba760bc5f29b6d3ab43f664297d99f5ef06966
blakeahalt <blakeahalt@gmail.com>
2022-06-28 22:17:16 -0700

    Update PhoneBook.js

--- part2/phonebook/components/PhoneBook.js
+++ part2/phonebook/components/PhoneBook.js
@@ -5,14 +5,14 @@ const PhoneBook = ({ persons, remove, update }) => (
     <div>
         <h2>Phone Book</h2>
 		<div key={persons.id} style={{ margin: '5px', padding: '5px' }}></div>
-            {persons.map(person => {
-                return (
-                    <Person 
-                        person={person}
-                        key={person.id}
-                        remove={remove}
-                        update={update}
-                    />
+			{persons.map(person => {
+				return (
+					<Person 
+						person={person}
+						key={person.id}
+						remove={remove}
+						update={update}
+					/>
                 )
             })}
     </div>
