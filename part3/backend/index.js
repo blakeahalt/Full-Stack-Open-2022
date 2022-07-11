f1370089bf978dd2ebf4b6c1e247eec659b49f93
blakeahalt <blakeahalt@gmail.com>
2022-07-11 03:19:20 -0700

    refs/heads/main ?

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,28 +1,177 @@
+// const express = require('express')
+// const morgan = require('morgan')
+// const cors = require('cors')
+// const app = express()
+
+// app.use(express.static('build'))
+// app.use(express.json())
+// app.use(cors())
+
+// const requestLogger = (request, response, next) => {
+// 	console.log('Method:', request.method)
+// 	console.log('Path:  ', request.path)
+// 	console.log('Body:  ', request.body)
+// 	console.log('---')
+// 	next()
+//   }
+
+// app.use(requestLogger)
+
+// morgan.token('body', req => 
+// {
+//   return JSON.stringify(req.body)
+// })
+
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+// let persons =[
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ]
+
+// const getRandomInt = (min, max) => {
+// 	min = Math.ceil(1);
+// 	max = Math.floor(100);
+// 	return Math.floor(Math.random() * (max - min) + min); 
+//   }
+
+// const generateId =() => {
+//   return getRandomInt()
+// }
+
+// app.post('/api/persons/', (request, response) => {
+//   const body = request.body
+//   const foundPerson = persons.find(person => person.name === body.name)
+
+//   const person = {
+//       id: generateId(),
+//       name: body.name,
+//       number: body.number,
+//     }
+
+//     if (foundPerson) {
+//       return response.status(400).json({ 
+//         error: 'That name already exists' 
+//       })
+//     } else if (!body.name && !body.number) {
+//         return response.status(400).json({ 
+//           error: 'No name or number' 
+//         })
+//     } else if (!body.number) {
+//         return response.status(400).json({ 
+//           error: 'No number' 
+//         })
+//     } else if (!body.name) {
+//         return response.status(400).json({ 
+//           error: 'No name' 
+//         })
+//     }
+
+//     persons = persons.concat(person)
+//     response.json(person)
+    
+//   })
+
+// app.delete('/api/persons/:id/', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(person => person.id !== id)
+
+//   response.status(204).end()
+// })
+
+
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const body = persons.find(person => person.id === id)
+
+//   if (body) {
+//     response.json(body)
+//     morgan.token('body', req => 
+// {
+//   return JSON.stringify(req.body)
+// })
+//   } else {
+//     return (
+//       response.send("HTTP Error 404: Not Found"),
+//       response.status(404).end()
+//       )
+//   }
+// })
+
+// app.get('/api/persons', (request, response) => {
+//   const id = Number(request.params.id)
+
+//     response.json(persons)
+//   })
+
+
+// app.get('/info', (request, response) => {
+// 	// const infoData = {
+// 	//   content: `Phonebook has info for ${persons.length} people.`,
+// 	//   date: Date()
+
+// 	const dateInfo = new Date()
+// 	response.send(`<h2>Phonebook has info for ${persons.length} people</h2><h2>${dateInfo}</h2>`)
+// 	response.json(infoData)
+//   })
+
+
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: 'unknown endpoint' })
+// }
+
+// app.use(unknownEndpoint)
+
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const User = require('./models/person')
+const cors = require('cors')
 
-app.use(express.static('build'))
+// const morgan = require('morgan')
+
+// const requestLogger = (request, response, next) => {
+// 	console.log('Method:', request.method)
+// 	console.log('Path:  ', request.path)
+// 	console.log('Body:  ', request.body)
+// 	console.log('---')
+// 	next()
+//   }
+
 app.use(express.json())
+// app.use(requestLogger)
 app.use(cors())
+app.use(express.static('build'))
 
-const requestLogger = (request, response, next) => {
-	console.log('Method:', request.method)
-	console.log('Path:  ', request.path)
-	console.log('Body:  ', request.body)
-	console.log('---')
-	next()
-  }
+// morgan.token('body', req => 
+// {
+//   return JSON.stringify(req.body)
+// })
 
-app.use(requestLogger)
-
-morgan.token('body', req => 
-{
-  return JSON.stringify(req.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons =[
   { 
@@ -44,8 +193,43 @@ let persons =[
     "id": 4,
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
+  },
+  { 
+    "id": 6,
+    "name": "Blake", 
+    "number": "508-299-9373"
   }
 ]
+
+// const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+// const url = `mongodb+srv://blakeahalt:${password}@cluster0.nrpgtan.mongodb.net/Person?retryWrites=true&w=majority`
+
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// })
+
+// const User = mongoose.model('User', userSchema)
+
+// userSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
 
 const getRandomInt = (min, max) => {
 	min = Math.ceil(1);
@@ -59,13 +243,13 @@ const generateId =() => {
 
 app.post('/api/persons/', (request, response) => {
   const body = request.body
-  const foundPerson = persons.find(person => person.name === body.name)
+  const foundPerson = User.find(user => user.name === body.name)
 
-  const person = {
+  const user = new User ({
       id: generateId(),
-      name: body.name,
-      number: body.number,
-    }
+      name: user.name,
+      number: user.number,
+    })
 
     if (foundPerson) {
       return response.status(400).json({ 
@@ -85,9 +269,12 @@ app.post('/api/persons/', (request, response) => {
         })
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    // persons = persons.concat(person)
+    // response.json(person)
     
+    user.save().then(savedUser => {
+      response.json(savedUser)
+    })
   })
 
 app.delete('/api/persons/:id/', (request, response) => {
@@ -99,38 +286,38 @@ app.delete('/api/persons/:id/', (request, response) => {
 
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const body = persons.find(person => person.id === id)
+  // const id = Number(request.params.id)
+//   const body = persons.find(person => person.id === id)
 
-  if (body) {
-    response.json(body)
-    morgan.token('body', req => 
-{
-  return JSON.stringify(req.body)
-})
-  } else {
-    return (
-      response.send("HTTP Error 404: Not Found"),
-      response.status(404).end()
-      )
-  }
+//   if (body) {
+//     response.json(body)
+//     morgan.token('body', req => 
+// {
+//   return JSON.stringify(req.body)
+// })
+//   } else {
+//     return (
+//       response.send("HTTP Error 404: Not Found"),
+//       response.status(404).end()
+//       )
+//   }
+  User.findById(request.params.id).then(user => {
+    response.json(user)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-  const id = Number(request.params.id)
-
+  // const id = Number(request.params.id)
+  // response.json(persons)
+  User.find({}).then(user => {
     response.json(persons)
   })
-
+})
 
 app.get('/info', (request, response) => {
-	// const infoData = {
-	//   content: `Phonebook has info for ${persons.length} people.`,
-	//   date: Date()
-
 	const dateInfo = new Date()
 	response.send(`<h2>Phonebook has info for ${persons.length} people</h2><h2>${dateInfo}</h2>`)
-	response.json(infoData)
+	// response.json(info)
   })
 
 
@@ -140,7 +327,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,39 +1,58 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-       console.log('Please provide the password as an argument: node mongo.js <password>')
-       process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
+const userName = process.argv[3]
+const userNumber = process.argv[4]
 
-const url = `mongodb+srv://notes-app-full:${password}@cluster1.lvvbt.mongodb.net/?retryWrites=true&w=majority`
+// const url = `mongodb+srv://blakeahalt:${password}@cluster0.nrpgtan.mongodb.net/?retryWrites=true&w=majority`
 
-const uri = "mongodb+srv://blakeahalt:Mongodbpassword3827@cluster0.nrpgtan.mongodb.net/?retryWrites=true&w=majority"
+const url = `mongodb+srv://blakeahalt:${password}@cluster0.nrpgtan.mongodb.net/Person?retryWrites=true&w=majority`
 
-const noteSchema = new mongoose.Schema({
-       content: String,
-       date: Date,
-       important: Boolean,
+// const uri = "mongodb+srv://blakeadmin:Dbpassword@cluster0.nrpgtan.mongodb.net/?retryWrites=true&w=majority"
+
+const userSchema = new mongoose.Schema({
+       name: String,
+       number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const User = mongoose.model('User', userSchema)
 
-mongoose
+if (process.argv.length === 3) {
+mongoose.connect(url)
+User.find({}).then(result => {
+	console.log("phonebook:")
+	result.forEach(user => {
+	  console.log(`${user.name} ${user.number}`)
+	})
+	mongoose.connection.close()
+  })
+}
+
+else if (process.argv.length === 4) {
+	console.log('Please provide a telephone number')
+	process.exit(2)
+}
+
+
+else if (process.argv.length === 5) {
+	mongoose
        .connect(url)
-       .then((result) => {
+       .then(result => {
               console.log('connected')
-
-              const note = new Note({
-                     content: 'HTML is Easy',
-                     date: new Date(),
-                     important: true,
+              const user = new User({
+                     name: process.argv[3],
+                     number: process.argv[4],
               })
-
-              return note.save()
+              return user.save()
        })
        .then(() => {
-              console.log('note saved!')
+              console.log(`added ${userName} number: ${userNumber} to phonebook`)
               return mongoose.connection.close()
        })
        .catch((err) => console.log(err))
+	}
