86f1c333302c9d31bd07d07e72251b0f0d6bf37a
blake <blakehalt@gmail.com>
2023-04-06 00:06:52 -0700

    updates

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -21,8 +21,10 @@ const AnecdoteList = () => {
      {anecdotes.sort((a, b) => b.votes - a.votes).map((anecdote, index) => (
         <div key={`anecdote_${anecdote.id}_${index}`} style={{ border: '1px solid black', padding: '5px', margin: '1vh', display: 'flex', flexDirection: 'row'}}>
           <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <VoteButton anecdote={anecdote}/>
-            <DeleteButton id={anecdote.id} content={anecdote.content} />
+            <div style={{display: 'flex', flexDirection: 'column' }}>
+              <VoteButton anecdote={anecdote}/>
+              <DeleteButton id={anecdote.id} content={anecdote.content} />
+            </div>
             <div style={{minWidth: '20px', margin: '10px', textAlign: 'center' }}>
               {anecdote.votes}
             </div>
